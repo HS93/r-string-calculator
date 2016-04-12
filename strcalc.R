edc6d460852aa9d9546f8b414f22c0f8bf888fda
Harry Sutton <harry_93@live.com.au>
2016-04-12 15:36:59 +1000

    add subtract operator

--- strcalc.R
+++ strcalc.R
@@ -1,12 +1,18 @@
-compute =function(input_string){
-  values=unlist(strsplit(input_string, ' '))
-  num0 = as.integer(values[1])
-  operator = values[2]
-  num1= as.integer(values[3])
-  if (operator== '+'){
+compute <- function(input_string){
+  values <- unlist(strsplit(input_string, ' '))
+  num0 <- as.integer(values[1])
+  operator <- values[2]
+  num1<- as.integer(values[3])
+  if (operator == '+'){
     return (num0+num1)
+  }else if (operator == '-'){
+    return(num0 - num1)
   }else{
     print('unknown operator')
-    return (0)
+    return (NA)
   }
 }
+
+
+
+
